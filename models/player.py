b00a737f0ea86f8dc3eca93ae1b5b67221fdccfb
d7o1d1s0 <d7sarmy@gmail.com>
2021-08-07 14:42:15 +0100

    implemented player class

--- models/player.py
+++ models/player.py
@@ -1,3 +1,4 @@
-# def Player(self, name, choice):
-#     self.name = name
-#     self.choice = choice
+class Player():
+    def __init__(self, name, choice):
+        self.name = name
+        self.choice = choice
--- models/player_list.py
+++ models/player_list.py
@@ -1,27 +1,23 @@
-# from models.player import Player
+from models.player import Player
 
-# player_1 = Player('Donald', 'rock')
-# player_2 = Player('Finlay', 'paper')
-# player_3 = Player('Michelle', 'scissors')
+def rps(player1, player2):
 
-def rps(choices):
-
-    if choices[0] == choices[1]:
+    if player1.choice == player2.choice:
         return 'draw'
-    if choices[0].lower() == 'rock':
-        if choices[1].lower() == 'scissors':
+    if player1.choice.lower() == 'rock':
+        if player2.choice() == 'scissors':
             return 'player 1 wins'
-        if choices[1].lower() == 'paper':
+        if player2.choice.lower() == 'paper':
             return 'player 2 wins'
 
-    if choices[0].lower() == 'paper':
-        if choices[1].lower() == 'rock':
+    if player1.choice.lower() == 'paper':
+        if player2.choice.lower() == 'rock':
             return 'player 1 wins'
-        if choices[1].lower() == 'scissors':
+        if player2.choice.lower() == 'scissors':
             return 'player 2 wins'
 
-    if choices[0].lower() == 'scissors':
-        if choices[1].lower() == 'paper':
+    if player1.choice.lower() == 'scissors':
+        if player2.choice.lower() == 'paper':
             return 'player 1 wins'
-        if choices[1].lower() == 'rock':
+        if player2.choice.lower() == 'rock':
             return 'player 2 wins'
